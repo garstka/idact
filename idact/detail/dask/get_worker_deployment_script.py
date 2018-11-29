b4c56e09c52906e331348e23dcc50a2915752d1f
Matt Garstka <matt@garstka.net>
2018-11-29 02:05:04 +0100

    Revert the change to bind workers to 127.0.0.1
     - It results in "ERROR - Workers don't have promised key: ['tcp://127.0.0.1:(...)".

--- idact/detail/dask/get_worker_deployment_script.py
+++ idact/detail/dask/get_worker_deployment_script.py
@@ -33,7 +33,7 @@ def get_worker_deployment_script(scheduler_address: str,
         get_command_to_append_local_bin(),
         'dask-worker'
         ' {scheduler_address}'
-        ' --host 127.0.0.1'
+        ' --host 0.0.0.0'
         ' --bokeh'
         ' --bokeh-port {bokeh_port}'
         ' --nanny'
